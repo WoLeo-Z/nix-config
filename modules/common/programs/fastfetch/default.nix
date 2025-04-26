26446a98c18b213dbadd96e92c0c13c6d75d2869
WoLeo-Z <me@w0l.top>
2025-04-26 16:00:18 +0800

    feat: add fastfetch

---
+++ modules/common/programs/fastfetch/default.nix
@@ -0,0 +1,28 @@
+{
+  home.programs.fastfetch = {
+    enable = true;
+
+    settings = {
+      # display = {
+      #   color = {
+      #     keys = "35";
+      #     output = "95";
+      #   };
+      #   separator = " ➜  ";
+      # };
+
+      # logo = {
+      #   # Logo Source: https://github.com/NixOS/nixos-artwork/issues/50
+      #   # Svg: https://files.artturin.com/files/nixoscolorful.svg
+      #   source = ./logo_512x512.png;
+      #   type = "kitty-direct";
+      #   height = 10;
+      #   width = 20;
+      #   padding = {
+      #     top = 2;
+      #     left = 2;
+      #   };
+      # };
+    };
+  };
+}
