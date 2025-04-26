8f92b35a8e4950d4d5e5e06b15a3ac6d3a2fb3eb
WoLeo-Z <me@w0l.top>
2025-04-26 21:36:23 +0800

    refactor(lib): collectModules filter out "_"

--- lib/collectModules.nix
+++ lib/collectModules.nix
@@ -1,15 +1,23 @@
 { lib }:
 
 let
-  inherit (lib) attrNames filterAttrs hasSuffix flatten;
+  inherit (lib) attrNames filterAttrs hasSuffix flatten hasPrefix;
 in
 rec {
   collectModules = dir:
     let
       contents = builtins.readDir dir;
 
-      nixFiles = filterAttrs (name: type: type == "regular" && hasSuffix ".nix" name) contents;
-      subdirs = filterAttrs (name: type: type == "directory") contents;
+      nixFiles = filterAttrs (name: type:
+        type == "regular" &&
+        hasSuffix ".nix" name &&
+        !(hasPrefix "_" name)
+      ) contents;
+
+      subdirs = filterAttrs (name: type:
+        type == "directory" &&
+        !(hasPrefix "_" name)
+      ) contents;
 
       imported = map (name: dir + "/${name}") (attrNames nixFiles);
       nested = flatten (map (name: collectModules (dir + "/${name}")) (attrNames subdirs));
