d39a59231c9e347797c4b0e17264e1f43f07ba12
WoLeo-Z <me@w0l.top>
2025-05-05 21:58:44 +0800

    refactor(nix): remove unused substituters

--- modules/common/nix/nix.nix
+++ modules/common/nix/nix.nix
@@ -108,29 +108,27 @@
         # Cache
         "https://cache.ngi0.nixos.org" # content addressed nix cache (TODO)
         "https://cache.nixos.org" # funny binary cache
-        "https://nixpkgs-wayland.cachix.org" # automated builds of *some* wayland packages
+        # "https://nixpkgs-wayland.cachix.org" # automated builds of *some* wayland packages
         "https://nix-community.cachix.org" # nix-community cache
-        "https://hyprland.cachix.org" # hyprland
-        "https://nixpkgs-unfree.cachix.org" # unfree-package cache
-        "https://numtide.cachix.org" # another unfree package cache
-        "https://anyrun.cachix.org" # anyrun program launcher
-        "https://neovim-flake.cachix.org" # a cache for my neovim flake
+        # "https://nixpkgs-unfree.cachix.org" # unfree-package cache
+        # "https://numtide.cachix.org" # another unfree package cache
+        # "https://anyrun.cachix.org" # anyrun program launcher
+        # "https://neovim-flake.cachix.org" # a cache for my neovim flake
         "https://cache.garnix.io" # garnix binary cache, hosts prismlauncher
-        "https://ags.cachix.org" # ags
+        # "https://ags.cachix.org" # ags
       ];
 
       trusted-public-keys = [
         "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-        "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
+        # "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        # "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        # "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        # "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+        # "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
       ];
     };
   };
