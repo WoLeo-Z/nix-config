28a2d0da01b6ae11f2644d96f6711e8df38ff2c5
WoLeo-Z <me@w0l.top>
2025-05-05 21:58:15 +0800

    refactor: disable man man.generateCaches

--- modules/common/programs/fish.nix
+++ modules/common/programs/fish.nix
@@ -11,6 +11,8 @@
     '';
   };
 
+  documentation.man.generateCaches = false; # Disable to save time when rebuilding
+
   # Fix: cannot open database `.../programs.sqlite' at /run/current-system/sw/bin/command-not-found line 13.
   programs.command-not-found.enable = false;
 }
