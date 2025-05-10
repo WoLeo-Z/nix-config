67553e7299589ef2434ebf23ffeda976873d905a
WoLeo-Z <me@w0l.top>
2025-05-10 14:51:48 +0800

    feat: add btop

---
+++ modules/common/programs/btop.nix
@@ -0,0 +1,12 @@
+{
+  hm = {
+    programs.btop = {
+      enable = true;
+      # settings = {
+      #   theme = "Catppuccin";
+      # };
+    };
+
+    stylix.targets.btop.enable = true;
+  };
+}
