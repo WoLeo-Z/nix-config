a1c7307a5a08db83b28f80f74afb91ff935dc719
WoLeo-Z <45914900+WoLeo-Z@users.noreply.github.com>
2025-05-25 21:17:25 +0800

    feat(steam): add gamemode

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -19,7 +19,7 @@ in
       steam = {
         enable = true;
         remotePlay.openFirewall = true;
-        extraCompatPackages = [ pkgs.proton-ge-bin ];
+        # extraCompatPackages = [ pkgs.proton-ge-bin ];
         gamescopeSession.enable = true;
       };
 
@@ -31,6 +31,28 @@ in
           "--expose-wayland"
         ];
       };
+
+      gamemode = {
+        enable = true;
+        settings = {
+          general = {
+            softrealtime = "auto";
+            renice = 15;
+          };
+        };
+      };
+    };
+
+    environment.systemPackages = with pkgs; [ mangohud ];
+
+    hm.xdg.desktopEntries.steam-gamescope = {
+      name = "Steam Gamescope";
+      exec = "gamemoderun gamescope -W 1920 -H 1080 -w 1920 -h 1080 -f -e -- steam -gamepadui";
+      icon = "steam";
+      terminal = false;
+      type = "Application";
+      categories = [ "Game" ];
+      # startupNotify = true;
     };
 
     # Better for steam proton games
