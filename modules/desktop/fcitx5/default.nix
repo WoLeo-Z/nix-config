dcecc36d87cb6892f5b1fbcd5285c09da14ce7b3
WoLeo-Z <45914900+WoLeo-Z@users.noreply.github.com>
2025-05-27 21:57:45 +0800

    feat(fcitx5): use `rime-ice`, add `fcitx5-mellow-themes`, update conf

--- modules/desktop/fcitx5/default.nix
+++ modules/desktop/fcitx5/default.nix
@@ -6,33 +6,6 @@
 }:
 let
   cfg = config.modules.desktop.fcitx5;
-
-  # https://github.com/xddxdd/nixos-config/blob/3704ff233cde3452040b24e86283b164d605f7ca/nixos/client-apps/fcitx/rime-lantian-custom.nix
-  fcitx5-rime-with-addons =
-    (pkgs.fcitx5-rime.override {
-      librime = pkgs.nur.repos.xddxdd.lantianCustomized.librime-with-plugins;
-      rimeDataPkgs = with pkgs; [
-        nur.repos.xddxdd.rime-aurora-pinyin
-        nur.repos.xddxdd.rime-custom-pinyin-dictionary
-        nur.repos.xddxdd.rime-dict
-        nur.repos.xddxdd.rime-ice
-        nur.repos.xddxdd.rime-moegirl
-        nur.repos.xddxdd.rime-zhwiki
-        rime-data
-      ];
-    }).overrideAttrs
-      (old: {
-        # Prebuild schema data
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.parallel ];
-        postInstall =
-          (old.postInstall or "")
-          + ''
-            for F in $out/share/rime-data/*.schema.yaml; do
-              echo "rime_deployer --compile "$F" $out/share/rime-data $out/share/rime-data $out/share/rime-data/build" >> parallel.lst
-            done
-            parallel -j$(nproc) < parallel.lst || true
-          '';
-      });
 in
 {
   options.modules.desktop.fcitx5 = {
@@ -40,6 +13,9 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # to use svg themes
+    programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
+
     i18n = {
       defaultLocale = lib.mkDefault "en_US.UTF-8";
       supportedLocales = lib.mkDefault [
@@ -52,8 +28,15 @@ in
         fcitx5 = {
           waylandFrontend = true;
           addons = with pkgs; [
-            fcitx5-gtk
-            fcitx5-rime-with-addons
+            fcitx5-mellow-themes
+            (fcitx5-rime.override {
+              rimeDataPkgs = [
+                rime-data
+                nur.repos.xddxdd.rime-ice
+                nur.repos.xddxdd.rime-zhwiki
+                nur.repos.xddxdd.rime-moegirl
+              ];
+            })
           ];
           # plasma6Support = true;
         };
@@ -106,6 +89,11 @@ in
           "${config.home'.configDir}/fcitx5/config" \
           "${config.home'.configDir}/fcitx5/conf"
       '';
+
+      home.file."${config.home'.dataDir}/fcitx5/rime" = {
+        source = ./rime;
+        recursive = true;
+      };
     };
   };
 }
