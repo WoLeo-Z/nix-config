42dddd1e2d3d03a0a9c064eaae4a8e9eb331e9c4
WoLeo-Z <45914900+WoLeo-Z@users.noreply.github.com>
2025-06-11 15:10:20 +0800

    feat(boot): add kernelParams and availableKernelModules

--- modules/common/system/boot.nix
+++ modules/common/system/boot.nix
@@ -9,6 +9,26 @@ with lib;
     # kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelPackages = pkgs.linuxPackages_cachyos-lto;
 
+    kernelParams = [
+      "nowatchdog"
+      "modprobe.blacklist=iTCO_wdt" # watchdog for Intel
+      "modprobe.blacklist=sp5100_tco" # watchdog for AMD
+      "pcie_aspm=off" # Fix: log spam: PCIe Bus Error ...
+    ];
+
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "nvme"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "btrfs"
+      ];
+      kernelModules = [ ];
+    };
+
     # Bootloader: Grub
     loader = {
       timeout = 3;
