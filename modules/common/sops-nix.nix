fc8065c2881a7214d197ff029c626511244983de
WoLeo-Z <45914900+WoLeo-Z@users.noreply.github.com>
2025-06-12 21:32:17 +0800

    nfc(sops-nix): add SSH Key Preparation

--- modules/common/sops-nix.nix
+++ modules/common/sops-nix.nix
@@ -19,10 +19,23 @@
   sops = {
     # defaultSopsFile = "${inputs.nix-secrets}/secrets.yaml";
     age = {
+      # This will automatically import SSH keys as age keys
       sshKeyPaths = [
         "/etc/ssh/ssh_host_ed25519_key"
         "${config.user.home}/.ssh/id_ed25519"
       ];
+      # This is using an age key that is expected to already be in the filesystem
+      # keyFile = "/var/lib/sops-nix/key.txt";
+      # This will generate a new key if the key specified above does not exist
+      # generateKey = false;
     };
   };
+
+  # SSH Key Preparation
+  # When you run "sops [file]", you may get "age: no identity matched any of the recipients"
+  # Solotion:
+  #
+  # mkdir ~/.config/sops/age
+  # nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt"
+  # chmod 600 ~/.config/sops/age/keys.txt
 }
