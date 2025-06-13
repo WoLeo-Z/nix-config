2d280401712365c8c9ba6d5ab01f30e733e28179
WoLeo-Z <me@w0l.top>
2025-06-13 16:48:50 +0800

    refactor(nh): update flake path -> ~/nix-config

--- modules/common/nix/nh.nix
+++ modules/common/nix/nh.nix
@@ -5,6 +5,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "${config.user.home}/flake";
+    flake = "${config.user.home}/nix-config";
   };
 }
