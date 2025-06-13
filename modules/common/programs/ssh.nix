41430aeda39a2a00ced1b47117f6283ab68e8d53
WoLeo-Z <me@w0l.top>
2025-06-13 16:21:11 +0800

    feat(ssh): add ssh

---
+++ modules/common/programs/ssh.nix
@@ -0,0 +1,21 @@
+{
+  hm = {
+    programs.ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
+      matchBlocks = {
+        "github.com" = {
+          # "Using SSH over the HTTPS port for GitHub"
+          # "(port 22 is banned by some proxies / firewalls)"
+          hostname = "ssh.github.com";
+          port = 443;
+          user = "git";
+
+          # Specifies that ssh should only use the identity file explicitly configured above
+          # required to prevent sending default identity files first.
+          identitiesOnly = true;
+        };
+      };
+    };
+  };
+}
