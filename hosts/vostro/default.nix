50bbd8984b3a1ac0bbdaaaa0307baf3e0e9fe7a0
WoLeo-Z <me@w0l.top>
2025-06-24 15:50:57 +0800

    feat(kitty): add kitty

--- hosts/vostro/default.nix
+++ hosts/vostro/default.nix
@@ -19,6 +19,7 @@
             default = "alacritty";
             alacritty.enable = true;
             foot.enable = true;
+            kitty.enable = true;
           };
           nautilus.enable = true;
           browsers = {
---
+++ modules/desktop/apps/terminal/kitty.nix
@@ -0,0 +1,33 @@
+{ lib, config, ... }:
+
+with lib;
+let
+  cfg = config.modules.desktop.apps.terminal.kitty;
+in
+{
+  options.modules.desktop.apps.terminal.kitty = {
+    enable = mkEnableOption' { };
+  };
+
+  config = mkIf cfg.enable {
+    hm = {
+      programs.kitty = {
+        enable = true;
+        enableGitIntegration = true;
+        settings = {
+          window_padding_width = "3 5";
+          # background_blur = 64; # not working
+          # dim_opacity = 0.8; # increase dim opacity # FIXME: not working
+        };
+        keybindings = {
+          # Switch Ctrl+C & Ctrl+Shift+C
+          "ctrl+c" = "copy_to_clipboard";
+          "ctrl+v" = "paste_from_clipboard";
+          "ctrl+shift+c" = "send_text all \\u0003"; # send SIGINT
+        };
+      };
+
+      stylix.targets.kitty.enable = true;
+    };
+  };
+}
