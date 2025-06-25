1a05d589602901f5a09be0afbe573019cd1440f1
WoLeo-Z <me@w0l.top>
2025-06-25 16:25:42 +0800

    feat(btop): enable rocmSupport to fetch AMDGPU data

--- modules/profiles/hardware/gpu/amd.nix
+++ modules/profiles/hardware/gpu/amd.nix
@@ -13,5 +13,20 @@ mkMerge [
       amdvlk.support32Bit.enable = false;
       # opencl.enable = true;
     };
+
+    nixpkgs.overlays = [
+      (self: super: {
+        btop = super.btop.override {
+          # AMDGPU data is queried using the ROCm SMI library
+          # https://github.com/aristocratos/btop/blob/1c2ab3f5b57a623d7781f7286050d70b1892b760/README.md#gpu-compatibility
+          rocmSupport = true;
+        };
+      })
+    ];
+
+    hm.programs.btop.settings = {
+      # Impact performance, disable
+      rsmi_measure_pcie_speeds = false;
+    };
   })
 ]
