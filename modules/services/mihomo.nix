0474ee37a9398e22add53a54b430e95652fc3f17
WoLeo-Z <me@w0l.top>
2025-08-09 18:19:09 +0800

    feat(mihomo): add proxy environment variables

--- modules/services/mihomo.nix
+++ modules/services/mihomo.nix
@@ -35,6 +35,19 @@ in
       "127.0.0.88" = [ "dash.local" ]; # Dashboard
     };
 
+    environment.variables =
+      let
+        http_proxy = "http://127.0.0.1:7890";
+      in
+      {
+        http_proxy = http_proxy;
+        HTTP_PROXY = http_proxy;
+        https_proxy = http_proxy;
+        HTTPS_PROXY = http_proxy;
+        all_proxy = http_proxy;
+        ALL_PROXY = http_proxy;
+      };
+
     # # Encrypt whole yaml (binary format):
     # # sops encrypt /home/wol/.config/mihomo/config.yaml --input-type binary | save mihomo_config.yaml
     # sops.secrets.mihomo-config = {
