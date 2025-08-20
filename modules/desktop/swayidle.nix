dacbebf75019ad59d9c6d95312ab6ad53b3b6d00
WoLeo-Z <me@w0l.top>
2025-08-20 17:59:35 +0800

    feat(swayidle): auto-suspend-after-resume

--- modules/desktop/swayidle.nix
+++ modules/desktop/swayidle.nix
@@ -8,6 +8,14 @@
 with lib;
 let
   cfg = config.modules.desktop.swayidle;
+
+  # After resume, if Swaylock is not unlocked for 60 seconds, system will automatically suspend.
+  auto-suspend-after-resume = pkgs.writeShellScriptBin "auto-suspend-after-resume" ''
+    ${lib.getExe' pkgs.coreutils "sleep"} 60
+    if ${lib.getExe' pkgs.procps "pgrep"} "swaylock" > /dev/null; then
+      ${lib.getExe' pkgs.systemd "systemctl"} suspend
+    fi
+  '';
 in
 {
   options.modules.desktop.swayidle = {
@@ -34,6 +42,10 @@ in
             {
               event = "before-sleep";
               command = "${lib.getExe pkgs.swaylock} -fF";
+            }
+            {
+              event = "after-resume";
+              command = "${lib.getExe auto-suspend-after-resume}";
             }
           ];
           timeouts = [
