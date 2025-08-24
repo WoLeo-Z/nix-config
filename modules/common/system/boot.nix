a6f1e331469d89c9426d2326db8ea68e39f6e6b0
WoLeo-Z <me@w0l.top>
2025-08-24 19:10:26 +0800

    refactor(boot): change kernel to cachyos

--- modules/common/system/boot.nix
+++ modules/common/system/boot.nix
@@ -5,9 +5,9 @@ with lib;
   boot = {
     # Kernel
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages_xanmod_latest;
-    # kernelPackages = pkgs.linuxPackages_cachyos;
+    kernelPackages = pkgs.linuxPackages_cachyos;
     # kernelPackages = pkgs.linuxPackages_cachyos-lto;
     # kernelPackages = pkgs.linuxPackages_zen;
 
