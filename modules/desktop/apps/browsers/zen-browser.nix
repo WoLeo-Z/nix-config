de1ebf8037a68d873c50a757603348824ea2b41f
WoLeo-Z <me@w0l.top>
2025-09-19 20:56:21 +0800

    feat(zen-browser): add xdg.mimeApps

--- modules/desktop/apps/browsers/zen-browser.nix
+++ modules/desktop/apps/browsers/zen-browser.nix
@@ -28,6 +28,8 @@ in
           AutofillAddressesEnabled = false;
           AutoFillCreditCardEnabled = false;
           DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
           DisablePocket = true;
           DisableSetDesktopBackground = true;
           DisableTelemetry = true;
@@ -48,12 +50,50 @@ in
             StartPage = "homepage";
           };
           NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
           PasswordManagerEnabled = false;
           SearchBar = "unified";
           SearchSuggestEnabled = true;
           ShowHomeButton = false;
         };
       };
+
+      xdg.mimeApps =
+        let
+          value =
+            let
+              zen-browser = inputs.zen-browser.packages.${system}.twilight; # or beta
+            in
+            zen-browser.meta.desktopFileName;
+
+          associations = builtins.listToAttrs (
+            map
+              (name: {
+                inherit name value;
+              })
+              [
+                "application/x-extension-shtml"
+                "application/x-extension-xhtml"
+                "application/x-extension-html"
+                "application/x-extension-xht"
+                "application/x-extension-htm"
+                "x-scheme-handler/unknown"
+                "x-scheme-handler/mailto"
+                "x-scheme-handler/chrome"
+                "x-scheme-handler/about"
+                "x-scheme-handler/https"
+                "x-scheme-handler/http"
+                "application/xhtml+xml"
+                "application/json"
+                "text/plain"
+                "text/html"
+              ]
+          );
+        in
+        {
+          associations.added = associations;
+          defaultApplications = associations;
+        };
     };
   };
 }
