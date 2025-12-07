e34bfa7acb8ffa26230885e5c555f07238027c71
WoLeo-Z <me@w0l.top>
2025-12-07 13:25:20 +0800

    refactor(mihomo): use `networking.proxy` instead of env vars

--- modules/services/mihomo.nix
+++ modules/services/mihomo.nix
@@ -39,19 +39,10 @@ in
     #   "127.0.0.88" = [ "dash.local" ]; # Dashboard
     # };
 
-    environment.variables =
-      let
-        http_proxy = "http://127.0.0.1:7890";
-      in
-      {
-        http_proxy = http_proxy;
-        HTTP_PROXY = http_proxy;
-        https_proxy = http_proxy;
-        HTTPS_PROXY = http_proxy;
-        all_proxy = http_proxy;
-        ALL_PROXY = http_proxy;
-        NO_PROXY = "localhost,127.0.0.1,::1";
-      };
+    networking.proxy = {
+      default = lib.mkDefault "http://127.0.0.1:7890";
+      noProxy = lib.mkDefault "127.0.0.1,::1,localhost,.localdomain";
+    };
 
     # # Encrypt whole yaml (binary format):
     # # sops encrypt /home/wol/.config/mihomo/config.yaml --input-type binary | save mihomo_config.yaml
