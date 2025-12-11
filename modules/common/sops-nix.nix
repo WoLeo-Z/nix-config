403688a0aafcdc066459de8dd6a2b33d140ff316
WoLeo-Z <me@w0l.top>
2025-12-11 20:13:16 +0800

    refactor(sops-nix): refactor key usage

--- modules/common/sops-nix.nix
+++ modules/common/sops-nix.nix
@@ -2,6 +2,7 @@
   inputs,
   pkgs,
   config,
+  lib,
   ...
 }:
 {
@@ -13,26 +14,36 @@
     ssh-to-age
   ];
 
+  environment.variables = {
+    # this file is placed manually on new installs
+    SOPS_AGE_KEY_FILE = "/var/lib/sops-nix/key.txt";
+  };
+
   sops = {
     defaultSopsFile = "${inputs.nix-secrets}/secrets.yaml";
     age = {
       # This will automatically import SSH keys as age keys
-      sshKeyPaths = [
-        "/etc/ssh/ssh_host_ed25519_key"
-        "${config.user.home}/.ssh/id_ed25519"
-      ];
+      # sshKeyPaths = [
+      #   "/etc/ssh/ssh_host_ed25519_key"
+      #   "${config.user.home}/.ssh/id_ed25519"
+      # ];
+      sshKeyPaths = [ ];
       # This is using an age key that is expected to already be in the filesystem
-      # keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = config.environment.variables.SOPS_AGE_KEY_FILE;
       # This will generate a new key if the key specified above does not exist
-      # generateKey = false;
+      generateKey = false;
     };
   };
 
-  # SSH Key Preparation
-  # When you run "sops [file]", you may get "age: no identity matched any of the recipients"
-  # Solotion:
-  #
-  # mkdir ~/.config/sops/age
-  # nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt"
-  # chmod 600 ~/.config/sops/age/keys.txt
+  # the same key file as "/var/lib/sops-nix/key.txt"
+  # for editing secrets with user permission only
+  sops.secrets."sops-age-key-file" = {
+    sopsFile = "${inputs.nix-secrets}/secrets.yaml";
+    key = "sops-age-key-file"; # Specify the location of this secret
+    mode = "0600";
+    owner = config.user.name;
+  };
+  hm.xdg.configFile."sops/age/keys.txt".source =
+    lib.mkOutOfStoreSymlink
+      config.sops.secrets."sops-age-key-file".path;
 }
