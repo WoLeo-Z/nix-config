deba0e55c51c1ffb9236b2f29e442c7398d067af
WoLeo-Z <me@w0l.top>
2025-12-12 10:27:06 +0800

    feat(nixos): immutable /etc overlay, nixos-init, ...

--- modules/common/system/nixos.nix
+++ modules/common/system/nixos.nix
@@ -1,7 +1,6 @@
 { lib, pkgs, ... }:
 
 {
-  system.stateVersion = "24.11";
   time.timeZone = lib.mkDefault "Asia/Shanghai";
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
   # i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" ];
@@ -41,7 +40,33 @@
     nitch
   ];
 
-  stylix.targets.console.enable = true;
+  # Immutable /etc overlay
+  boot.initrd.systemd.enable = true;
+  services.userborn.enable = true;
+  system.etc.overlay = {
+    enable = true;
+    mutable = false;
+  };
+  environment.etc = {
+    "NIXOS".text = "";
+    "machine-id".text = "2a830309da54e3cc89231f02693ac238\n"; # `dbus-uuidgen`
+  };
+  # fix: avahi-daemon.service: Failed to set up special execution directory in /etc: Read-only file system
+  systemd.services.avahi-daemon.serviceConfig = {
+    ConfigurationDirectory = lib.mkForce [ ];
+  };
 
+  services.journald.extraConfig = ''
+    SystemMaxUse=1G
+  '';
+
+  services.dbus.implementation = "broker";
+
+  system.nixos-init.enable = true;
+
+  documentation.enable = false;
+  stylix.targets.console.enable = true;
   hardware.enableRedistributableFirmware = true;
+
+  system.stateVersion = "24.11";
 }
