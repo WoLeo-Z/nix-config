5e19f775eaaea4e2c90ccd0c79f53209a5647cb9
WoLeo-Z <me@w0l.top>
2025-12-21 14:38:12 +0800

    fix(safeeyes): finally fixed ...

--- modules/services/safeeyes.nix
+++ modules/services/safeeyes.nix
@@ -15,13 +15,26 @@ in
   };
 
   config = mkIf cfg.enable {
-    # we start safeeyes in window manager
-    # to fix "safeeyes: couldn't open display"
-    #
-    # services.safeeyes = {
-    #   enable = true;
-    # };
+    systemd.user.services.safeeyes = {
+      wantedBy = [ "graphical-session.target" ];
 
-    environment.systemPackages = with pkgs; [ safeeyes ];
+      unitConfig = {
+        Description = "Safe Eyes";
+        PartOf = [ "graphical-session.target" ];
+        StartLimitIntervalSec = 350;
+        StartLimitBurst = 30;
+      };
+
+      serviceConfig = {
+        ExecStart = lib.getExe pkgs.safeeyes;
+        Restart = "on-failure";
+        RestartSec = 3;
+
+        # HACK: prevent network errors (actually network shouldn't be required)
+        ExecStartPre = "${lib.getExe' pkgs.coreutils "sleep"} 3";
+        # HACK: ignore output noise "screen saver extension not supported"
+        StandardOutput = "null";
+      };
+    };
   };
 }
