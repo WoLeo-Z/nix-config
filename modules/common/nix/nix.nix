ef508a744c34315cce5747f581d344adb77bc766
WoLeo-Z <me@w0l.top>
2025-12-30 12:21:47 +0800

    chore: update flake.lock
    • Updated input 'caelestia-cli':
    'github:caelestia-dots/cli/8c83ae1e6a60de7d496d346bf3623f789a9f53e3'
    (2025-12-20) →
    'github:caelestia-dots/cli/f3ea42d43a07a13ba932d3b66515efbe51b3f532'
    (2025-12-29) • Updated input 'caelestia-cli/caelestia-shell':
    'github:caelestia-dots/shell/d819bf2fc3ded72ad4fa116c4f06c3afb7d59153'
    (2025-12-14) →
    'github:caelestia-dots/shell/dfa28d7f85215bd42d02717255c45b8126aeb8da'
    (2025-12-28) • Updated input 'caelestia-cli/caelestia-shell/quickshell':
    'git+https://git.outfoxxed.me/outfoxxed/quickshell?ref=refs/heads/master&rev=26531fc46ef17e9365b03770edd3fb9206fcb460'
    (2025-12-02) →
    'git+https://git.outfoxxed.me/outfoxxed/quickshell?ref=refs/heads/master&rev=41828c4180fb921df7992a5405f5ff05d2ac2fff'
    (2025-12-26) • Updated input 'caelestia-shell':
    'github:caelestia-dots/shell/66e509ae488b2c0468f2c803fd34c2625ca725dc'
    (2025-12-21) →
    'github:caelestia-dots/shell/dfa28d7f85215bd42d02717255c45b8126aeb8da'
    (2025-12-28) • Updated input 'caelestia-shell/caelestia-cli':
    'github:caelestia-dots/cli/8c83ae1e6a60de7d496d346bf3623f789a9f53e3'
    (2025-12-20) →
    'github:caelestia-dots/cli/b049cb17496b622917d41c057c78b3aa5d885a1a'
    (2025-12-26) • Updated input 'caelestia-shell/quickshell':
    'git+https://git.outfoxxed.me/outfoxxed/quickshell?ref=refs/heads/master&rev=26531fc46ef17e9365b03770edd3fb9206fcb460'
    (2025-12-02) →
    'git+https://git.outfoxxed.me/outfoxxed/quickshell?ref=refs/heads/master&rev=41828c4180fb921df7992a5405f5ff05d2ac2fff'
    (2025-12-26) • Updated input 'dank-material-shell':
    'github:AvengeMedia/DankMaterialShell/dca07a70f8269e3cb598095cf0b22e65e1c72082'
    (2025-12-20) →
    'github:AvengeMedia/DankMaterialShell/9501d66af65f89a9dff77bbb08dc9b9b7b9cf511'
    (2025-12-29) • Removed input 'dank-material-shell/dgop' • Removed input
    'dank-material-shell/dgop/nixpkgs' • Updated input
    'dank-material-shell/quickshell':
    'git+https://git.outfoxxed.me/quickshell/quickshell?ref=refs/heads/master&rev=26531fc46ef17e9365b03770edd3fb9206fcb460'
    (2025-12-02) →
    'git+https://git.outfoxxed.me/quickshell/quickshell?ref=refs/heads/master&rev=41828c4180fb921df7992a5405f5ff05d2ac2fff'
    (2025-12-26) • Updated input 'home-manager':
    'github:nix-community/home-manager/61fcc9de76b88e55578eb5d79fc80f2b236df707'
    (2025-12-21) →
    'github:nix-community/home-manager/d99b4ca5debaa082c7d76015aa2b7f3fc7e8b5f7'
    (2025-12-29) • Updated input 'nix-ai-tools':
    'github:numtide/nix-ai-tools/6ac434885b639b55296b5674d0145491535c6d26'
    (2025-12-20) →
    'github:numtide/nix-ai-tools/7bdb81f9ac6605019707fc32f7e11d348dc9a058'
    (2025-12-30) • Updated input 'nixpkgs':
    'github:NixOS/nixpkgs/c6245e83d836d0433170a16eb185cefe0572f8b8'
    (2025-12-18) →
    'github:NixOS/nixpkgs/c0b0e0fddf73fd517c3471e546c0df87a42d53f4'
    (2025-12-28) • Updated input 'nixpkgs-small':
    'github:NixOS/nixpkgs/cab52b36dca5a5a368f05bd03fca0b543b5b9a3e'
    (2025-12-20) →
    'github:NixOS/nixpkgs/d23fedd87fcd067b1d160323fae0d0e4f995527d'
    (2025-12-29) • Updated input 'nur':
    'github:nix-community/NUR/51d3347e63a34996f35275c77372eeafaa11e0ed'
    (2025-12-20) →
    'github:nix-community/NUR/bc4f03b0ecb04e381d98f4dbf57c2e4c1ef0bcb4'
    (2025-12-30) • Updated input 'sops-nix':
    'github:Mic92/sops-nix/443a7f2e7e118c4fc63b7fae05ab3080dd0e5c63'
    (2025-12-15) →
    'github:Mic92/sops-nix/61b39c7b657081c2adc91b75dd3ad8a91d6f07a7'
    (2025-12-28) • Updated input 'spicetify-nix':
    'github:Gerg-L/spicetify-nix/fa6a5dde9d95bf7b8f075ff5aceeb1d97fa9043a'
    (2025-12-14) →
    'github:Gerg-L/spicetify-nix/442accd416df2f85ada73937996a95749f9fdeb3'
    (2025-12-29) • Updated input 'stylix':
    'github:nix-community/stylix/e6829552d4bb659ebab00f08c61d8c62754763f3'
    (2025-12-16) →
    'github:nix-community/stylix/551df12ee3ebac52c5712058bd97fd9faa4c3430'
    (2025-12-24) • Updated input 'zen-browser':
    'github:0xc000022070/zen-browser-flake/4563d0ffdf586368f1bec1a39cc20c9b12b73884'
    (2025-12-20) →
    'github:0xc000022070/zen-browser-flake/fe8d1a61a904b336f453d7ab5ae7d691a21c5cbf'
    (2025-12-28)

--- modules/common/nix/nix.nix
+++ modules/common/nix/nix.nix
@@ -128,12 +128,14 @@
         "https://cache.nixos.org" # funny binary cache
         "https://nix-community.cachix.org" # nix-community cache
         "https://cache.garnix.io" # garnix binary cache, hosts prismlauncher
+        "https://cache.numtide.com" # numtide binary cache
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
   };
