5153e9703e1c1eef4986a33e2df97a4acc98ae28
WoLeo-Z <me@w0l.top>
2026-03-13 20:50:29 +0800

    fix(hardware): hardware.amdgpu.amdvlk removed

--- modules/profiles/hardware/gpu/amd.nix
+++ modules/profiles/hardware/gpu/amd.nix
@@ -8,9 +8,6 @@ mkMerge [
   (mkIf (any (s: hasPrefix "gpu/amd" s) hardware) {
     hardware.amdgpu = {
       initrd.enable = true;
-      # Disable AMDVLK to slightly improve performance (maybe?)
-      amdvlk.enable = false;
-      amdvlk.support32Bit.enable = false;
       # opencl.enable = true;
     };
 
